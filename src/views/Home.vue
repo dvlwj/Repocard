8b2b738d4cff125f3ea805ca13a4438d9878b129
dvlwj <dvlimweijiang@gmail.com>
2020-06-08 21:43:20 +0700

    change homepage to login page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-  </div>
+  <v-content>
+    <v-container class="fill-height" fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="12" sm="8" md="4">
+          <v-card class="elevation-12">
+            <v-toolbar color="primary" dark flat>
+              <v-toolbar-title>Selamat Datang !</v-toolbar-title>
+              <v-spacer></v-spacer>
+            </v-toolbar>
+            <v-card-text>
+              <v-form>
+                <v-text-field
+                  label="Nama Akun"
+                  name="login"
+                  prepend-icon="mdi-account"
+                  type="text"
+                ></v-text-field>
+
+                <v-text-field
+                  id="password"
+                  label="Kata Sandi"
+                  name="password"
+                  prepend-icon="mdi-lock"
+                  type="password"
+                ></v-text-field>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary">Masuk</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-content>
 </template>
 
 <script>
