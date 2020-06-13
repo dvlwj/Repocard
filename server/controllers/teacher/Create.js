e319a807167d52f9dc06d483feff16702e73adca
dvlwj <dvlimweijiang@gmail.com>
2020-06-14 01:18:20 +0700

    add create teacher

---
+++ server/controllers/teacher/Create.js
@@ -0,0 +1,29 @@
+const bcrypt = require('bcrypt');
+const response = require('../../shared/utils/response');
+const connection = require('../../shared/databases/connection');
+
+exports.create = function create(req, res) {
+  const requestPayloadBody = req.body;
+  console.log(`Mulai Create Guru Controller, payload : ${JSON.stringify(requestPayloadBody)}`);
+  const { username, password } = requestPayloadBody;
+  connection.query(
+    {
+      sql: 'INSERT INTO data_user (username, password, level, active) VALUES (?, md5(?), 2, 1)',
+      timeout: 30000,
+    },
+    [username, password, ],
+    (error, rows) => {
+      if (error || rows.length === 0) {
+        const message = 'Gagal Menambah Guru Baru';
+        response.error(message, res);
+      } else {
+        console.log('Sukses menambah guru baru');
+        const dataToReturn = {
+          message: 'Sukses menambah guru',
+        };
+        response.ok(dataToReturn, res);
+      }
+      console.log('Selesai Create Guru Controller');
+    },
+  );
+};
