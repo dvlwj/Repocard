aa9d40d542254ad958016522e1739b85a1ba4024
dvlwj <dvlimweijiang@gmail.com>
2020-06-14 01:22:50 +0700

    add Update teacher

--- server/controllers/teacher/Update.js
+++ server/controllers/teacher/Update.js
@@ -3,26 +3,26 @@ const connection = require('../../shared/databases/connection');
 
 exports.create = function create(req, res) {
   const requestPayloadBody = req.body;
-  console.log(`Mulai Create Guru Controller, payload : ${JSON.stringify(requestPayloadBody)}`);
+  console.log(`Mulai Update Guru Controller, payload : ${JSON.stringify(requestPayloadBody)}`);
   const { username, password } = requestPayloadBody;
   connection.query(
     {
-      sql: 'INSERT INTO data_user (username, password, level, active) VALUES (?, md5(?), 2, 1)',
+      sql: 'INSERT INTO data_user (username, password) VALUES (?, md5(?))',
       timeout: 30000,
     },
     [username, password],
     (error, rows) => {
       if (error || rows.length === 0) {
-        const message = 'Gagal Menambah Guru Baru';
+        const message = 'Gagal mengubah data guru';
         response.error(message, res);
       } else {
-        console.log('Sukses menambah guru baru');
+        console.log('Sukses mengubah data guru');
         const dataToReturn = {
-          message: 'Sukses menambah guru',
+          message: 'Sukses mengubah data guru',
         };
         response.ok(dataToReturn, res);
       }
-      console.log('Selesai Create Guru Controller');
+      console.log('Selesai Update Guru Controller');
     },
   );
 };
